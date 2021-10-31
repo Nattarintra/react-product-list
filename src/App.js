a8a6c64900871a2129e474a8c7e64bb0af445ecd
nattarintraTh <nattarintra@headofsearch.se>
2021-10-31 17:07:08 +0100

    Add : eslinetra file

--- src/App.js
+++ src/App.js
@@ -1,18 +1,10 @@
 import "./App.scss"
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <h1>React Product list</h1>
+    </>
   )
 }
-
 export default App
