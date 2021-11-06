cefbfcbece865cee923cc1997fd9911fc970dc9f
nattarintraTh <nattarintra@headofsearch.se>
2021-11-06 15:58:08 +0100

    Add : Pagination page.

--- src/components/product-card/ProductCard.jsx
+++ src/components/product-card/ProductCard.jsx
@@ -2,7 +2,7 @@ import React from "react"
 import { Link } from "react-router-dom"
 import { Card } from "react-bootstrap"
 
-const ProductCard = ({ filename = "/images/default.png", product_name = "Product name", brand_name = "Dyrberg/Kern", actual_price = "55.000", base_price = "55.000" }) => {
+const ProductCard = ({ filename = "/images/default.png", product_name = "Product name", brand_name = "Dyrberg/Kern", actual_price = "55.000" }) => {
   return (
     <>
       <Link to="#">
@@ -12,7 +12,6 @@ const ProductCard = ({ filename = "/images/default.png", product_name = "Product
             <Card.Text>{product_name}</Card.Text>
             <Card.Text>{brand_name}</Card.Text>
             <Card.Text>{actual_price}</Card.Text>
-            <Card.Text>{base_price}</Card.Text>
           </Card.Body>
         </Card>
       </Link>
--- src/views/ProductView/ProductItems.jsx
+++ src/views/ProductView/ProductItems.jsx
@@ -1,23 +1,37 @@
-import React from "react"
+import React, { useState } from "react"
 import { Container, Row, Col } from "react-bootstrap"
 import ProductCard from "@components/product-card/ProductCard"
-import RenderItems from "@components/render-items/RenderItems"
+
+//import RenderItems from "@components/render-items/RenderItems"
 import { productData } from "@data/product_list"
 
+import ReactPaginate from "react-paginate"
+
 const ProductItems = () => {
+  const [productItems] = useState(productData.slice(0, 44))
+  const [pageNumber, setPageNumber] = useState(0)
+
+  const productItemsPerPage = 8
+  const pagesVisited = pageNumber * productItemsPerPage
+
+  const displayProductItems = productItems.slice(pagesVisited, pagesVisited + productItemsPerPage).map(productItem => {
+    return (
+      <Col key={productItem.id} className="mt-5">
+        <ProductCard actual_price={productItem.actual_price} brand_name={productItem.brand_name} filename={`${productItem.filename}`} id={productItem.id} product_name={productItem.product_name} />
+      </Col>
+    )
+  })
+  const pageCount = Math.ceil(productItems.length / productItemsPerPage)
+
+  const changePage = ({ selected }) => {
+    setPageNumber(selected)
+  }
+
   return (
     <div className="">
       <Container>
-        <Row>
-          <RenderItems
-            items={productData}
-            render={({ id, product_name, brand_name, filename, actual_price, base_price }) => (
-              <Col>
-                <ProductCard actual_price={actual_price} base_price={base_price} brand_name={brand_name} filename={`${filename}`} id={id} product_name={product_name} />
-              </Col>
-            )}
-          />
-        </Row>
+        <Row>{displayProductItems}</Row>
+        <ReactPaginate activeClassName={"paginationActive"} containerClassName={"paginationBtns"} disabledClassName={"paginationDisabled"} nextLabel={"Next"} nextLinkClassName={"nextBtn"} pageCount={pageCount} previousLabel={"Previous"} previousLinkClassName={"previoseBtn"} onPageChange={changePage} />
       </Container>
     </div>
   )
